25c4e2601de14bc477fb608958282e6db224a293
Jc-Dent0n <62409717+Jc-Dent0n@users.noreply.github.com>
2020-06-13 01:35:08 +0300

    Create level1.java

---
+++ level1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class level1
+{
+    public static int squirrel (int N)
+    {
+        if(N == 0)
+        {
+            N = 1;
+        }
+        else if (N <= 12)
+        {
+            N = (int) (Math.round(Math.sqrt(2 * Math.PI * N) * (Math.pow((N / Math.E), N)) * Math.exp(1/((12 * N) + 0.75))));
+            String[] str_N = String.valueOf(N).split("");
+            N = Integer.valueOf(str_N[0], 10);
+        }
+        else
+        {
+            N = 2;
+        }
+
+        return N;
+    }
+}
