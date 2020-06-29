32458b40cd1078d3215c38bf9203d7fb1d404f7e
Jc-Dent0n <62409717+Jc-Dent0n@users.noreply.github.com>
2020-06-30 01:30:25 +0300

    Update sqr_round_war3.java

--- sqr_round_war3.java
+++ sqr_round_war3.java
@@ -1,57 +1,61 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class FOR_TESTS_TMP {
     public static void main(String[] args) {
-        int N_y = 6;
-        int M_x = 7;
-        int[] needles = {3,3,5,4,4,4,5,5,6,5};
+        int N_y = 5;
+        int M_x = 5;
+        int[] needles = {1,1,2,1,5,1,3,2,4,5};
         int[][] arr_sqr = new int[N_y][M_x];
         int Days = 0;
-        for (int i = 0; i < N_y; i++) {
-            for (int j = 0; j < M_x; j++) {
-                for(int k = 0; k < needles.length; k=k+2){
-                    Days = 1;
-                    if(needles[k] == i+1 && needles[k+1] == j+1){
-                        //System.out.println((i+1)+""+(j+1));
-                        arr_sqr[i][j] += Days;
-                    }
+        int Max_Days = N_y+M_x-1;
 
-                }
-                Days++;
-            }
-        }
+        boolean Flag = false;
 
-        for (int i = 0; i < N_y; i++) {
-            for (int j = 0; j < M_x; j++) {
-                if(arr_sqr[i][j] == 1) {
-                    arr_sqr[i][j]++;
-                }
-                if(i-1 >= 0 && j-1 >= 0){
-                    if(arr_sqr[i][j] >= 2){
-                        if(arr_sqr[i-1][j] == 0){
-                            arr_sqr[i-1][j] = 1;
-                        }
-                        if(arr_sqr[i][j-1] == 0){
-                            arr_sqr[i][j-1] = 1;
+        do{
+            Days++;
+
+            for (int i = 0; i < N_y; i++) {
+                for (int j = 0; j < M_x; j++) {
+                    for(int k = 0; k < needles.length; k=k+2){
+                        //Days = 1;
+                        if(needles[k] == i+1 && needles[k+1] == j+1){
+                            //System.out.println((i+1)+""+(j+1));
+                            arr_sqr[i][j] += 1;
+                            //System.out.println(arr_sqr[i][j]);
                         }
                     }
-                    else{
+                    if(i-1 >= 0){
+                        if(arr_sqr[i][j] >= 2) {
+                            if (arr_sqr[i - 1][j] == 0) {
+                                arr_sqr[i - 1][j]++;
+                            }
+                        }
                         if(arr_sqr[i-1][j] >= 2){
-                            arr_sqr[i][j] = 1;
+                            if(arr_sqr[i][j] != Days){
+                                arr_sqr[i][j]++;
+                                //System.out.println(arr_sqr[i][j] + " " + i + "" + j);
+                            }
                         }
-                        if(arr_sqr[i][j-1] >= 2){
-                            arr_sqr[i][j] = 1;
+                        if(arr_sqr[i-1][j] == arr_sqr[i][j]){
+                            System.out.println(arr_sqr[i-1][j] + " " + i + "" + j + "" + arr_sqr[i][j]);
                         }
                     }
+//                    if(j-1 >= 0){
+//                        if(arr_sqr[i][j-1] >= 2){
+//                            if(arr_sqr[i][j] != Days){
+//                                arr_sqr[i][j]++;
+//                                //System.out.println(arr_sqr[i][j] + " " + i + "" + j);
+//                            }
+//                        }
+//                    }
                 }
             }
-        }
 
+        }while(Days < 2);
+
+        System.out.println(arr_sqr[3][0]);
         System.out.println("\n\n");
-
-
-
-
 
         for (int[] anArr : arr_sqr) {
             for (int anAnArr : anArr) {
@@ -59,6 +63,5 @@ public class FOR_TESTS_TMP {
             }
             System.out.println();
         }
-       // System.out.println(Days);
     }
 }
